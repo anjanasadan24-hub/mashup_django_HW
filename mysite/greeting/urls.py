a137feb6a5e89153e500bba1605d4634e465cf24
Anjana <anjanasadan24@gmail.com>
2026-03-26 22:29:03 +0530

    commit

--- mysite/greeting/urls.py
+++ mysite/greeting/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [  
     path('', views.home, name='home'),  
-    path('about/', views.about, name='about'),  
-]
+    path('about/', views.about, name='about'),
+    
+    path('employees/', views.employee_list, name='employees'), 
+    path('students/', views.student_list, name='students'),
+    path('form/',views.form,name='form'),
+    path('result/', views.result, name='result'),
+
+    path('vistor/',views.vistor,name='vistor'),
+    path('results/', views.results, name='results'),]
--- mysite/greeting/views.py
+++ mysite/greeting/views.py
@@ -8,7 +8,44 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def employee_list(request):
+    employees = [
+        {"name": "Anjana", "job": "Developer", "salary": 50000, "full_time": True},
+        {"name": "Rahul", "job": "Designer", "salary": 30000, "full_time": False},
+        {"name": "Meena", "job": "Manager", "salary": 70000, "full_time": True},
+    ]
 
+    return render(request, 'emp.html', {"employees": employees})
+def student_list(request):
+    students = [
+        {"name": "Anjana", "grade": 85, "passed": True},
+        {"name": "Rahul", "grade": 40, "passed": False},
+        {"name": "Meena", "grade": 72, "passed": True},
+    ]
 
+    return render(request, 'students.html', {"students": students})
 
-# Create your views here.
+def form(request):
+    return render(request, 'form.html')
+
+def result(request):
+    username = request.GET.get('username', '')  # get data from GET request
+    
+    return render(request, 'result.html', {
+        'username': username,
+        'data': request.GET
+    })
+def vistor(request):
+    return render(request, 'vistor.html')
+def results(request):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            color = request.POST.get('color')
+    
+            return render(request, 'results.html',{
+            'name': name,
+            'color': color,
+            'data': request.POST
+            })
+
+        return render(request, 'vistor.html')    
